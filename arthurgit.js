4c7287cb2e294b9a3472833e1e957faa6ca4ad54
Arthur <arthuroli2806@gmail.com>
2025-05-17 00:38:37 -0300

    Add files via upload

---
+++ arthurgit.js
@@ -0,0 +1,11 @@
+function Home () {
+    return (
+        <div>
+            <h1>Welcome to Arthur's Page</h1>
+            <h2>Arthur TEST </h2>
+            <h3> Olá </h3>
+        </div>
+    );
+}
+
+export default Home;  
